04c70e1bea712750c75ddd6b770ebbc25a18d2c8
Gevorg Manukyan <gevorg.manukyan@yahoo.com>
2025-01-11 00:34:32 +0400

    making migrations

---
+++ database/migrations/2025_01_10_200237_create_cars_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('model');
+            $table->foreignId('comfort_category_id')->constrained('comfort_categories')->onDelete('cascade');
+            $table->foreignId('driver_id')->constrained('employees')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
