224305d29f53fef2ea4bfd1aff0fa3b933d3986d
Gevorg Manukyan <gevorg.manukyan@yahoo.com>
2025-01-12 20:54:27 +0400

    migration fix

--- database/migrations/2025_01_10_195815_create_employees_table.php
+++ database/migrations/2025_01_10_195815_create_employees_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('position');
+            $table->foreignId('position_id')->constrained('positions')->onDelete('cascade');
             $table->timestamps();
         });
     }
